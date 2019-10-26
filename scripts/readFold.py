0db582704247f11a9fc4e6cac289852698139c09
Italo Oliveira <38302009+arkanttus@users.noreply.github.com>
2019-10-26 18:41:16 -0500

    Fixed adress directory

--- scripts/readFold.py
+++ scripts/readFold.py
@@ -1,7 +1,7 @@
 from numpy import *
 
 def readFold(idFold, typeSet):
-    filename = "C:/Users/italo/Documents/pibic/Material Italo - v1/colecao/codes-to-create-school-wiki2014/virtual_folds/fixed/" + typeSet + '_' + str(idFold) + '.txt'
+    filename = "../colecao/codes-to-create-school-wiki2014/virtual_folds/fixed/" + typeSet + '_' + str(idFold) + '.txt'
 
     fileId = open(filename, 'r')
     In = loadtxt(fileId, delimiter=" ", dtype='int')
